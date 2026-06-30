bf5a4b94abc97cbb4e2fbce0d35f63f8622fffd9
Mirza Rayana Sanzana <sanzanarayana@gmail.com>
2026-06-30 16:28:17 +0800

    Initial commit

---
+++ smart_trolley.py
@@ -0,0 +1,16 @@
+# Simple Smart Trolley
+# Final Project for Python 101
+
+# In this project, we will build a small shopping trolley system.
+
+# The program will:
+# 1. Store shopping items
+# 2. Store item prices
+# 3. Store item quantities
+# 4. Calculate subtotals
+# 5. Calculate the total bill
+# 6. Apply discounts
+# 7. Display a receipt
+# 8. Save the receipt to a text file
+# 9. Add error handling
+# 10. Turn the project into a Streamlit app later
